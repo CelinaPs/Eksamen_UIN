b032994b98e30a7e66e1d4c7873ce8c0c9ddac50
Martehalv <martehal@hiof.no>
2025-04-24 11:30:05 +0200

    fikset route i app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,19 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./components/Home";
 
 function App() {
   const [count, setCount] = useState(0);
 
-  return <></>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;

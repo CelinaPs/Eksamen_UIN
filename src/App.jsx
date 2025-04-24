ef4abb39bb6116c2fe20f35b651b014e2d892ad9
Martehalv <martehal@hiof.no>
2025-04-24 11:41:49 +0200

    gjort klar filene til å begynne å skrive inn kode

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,9 @@ import viteLogo from "/vite.svg";
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
+import EventPage from "./components/EventPage";
+import CategoryPage from "./components/CategoryPage";
+import Dashboard from "./components/Dashboard";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -12,6 +15,9 @@ function App() {
     <Router>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/event/:id" element={<EventPage />} />
+        <Route path="/category/:slug" element={<CategoryPage />} />
+        <Route path="/dashboard" element={<Dashboard />} />
       </Routes>
     </Router>
   );

a175b62c2154b3de34b025345680cad40edefa12
aynazar1202222 <ainia@hiof.no>
2025-05-16 18:29:14 +0200

    lagde artistcard og fikset Eventpage
    lag

--- src/components/EventPage.jsx
+++ src/components/EventPage.jsx
@@ -2,36 +2,40 @@ import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 
 export default function EventPage() {
+  // henter id parameteren fra URLen
   const { id } = useParams();
   const [event, setEvent] = useState(null);
 
-  const fetchEvent = async () => {
-    const API_KEY = "5OYpmz3EGOfKvtU8vlLA3cC6ASbAeGgE"; //API-key
-    const response = await fetch(
-      `https://app.ticketmaster.com/discovery/v2/events/${id}.json?apikey=${API_KEY}`
-    );
-    const data = await response.json(); //Henter on konverterer JSON data fra TIcketmaster API
-    setEvent(data); //Lagrer data i event variabel
+  const getEvent = async () => {
+    const API_KEY = "5OYpmz3EGOfKvtU8vlLA3cC6ASbAeGgE";
+
+    fetch(
+      `https://app.ticketmaster.com/discovery/v2/attractions/${id}.json?apikey=${API_KEY}`
+    )
+      .then((res) => res.json())
+      .then((data) => setEvent(data))
+      .catch((error) =>
+        console.error("Det skjedde noe rart ved henting av event")
+      );
   };
 
   useEffect(() => {
-    fetchEvent(); //Kjører når komponenten vises
+    getEvent();
+    console.log(event);
   }, [id]);
 
-  if (!event) return <p>Fant ikke eventet eller laster fortsatt...</p>; //Viser melding mens vi venter på "event"
+  const image = event?.images?.[0]?.url;
 
   return (
-    <div>
-      <h1>{event.name}</h1>
-      <p>{event.dates?.start?.localDate}</p>
-      {event.images?.[0]?.url && (
-        <img src={event.images[0].url} alt={event.name} width="300" />
+    <section className="eventdetaljer">
+      {event ? (
+        <>
+          <h2>{event.name}</h2>
+          {image && <img src={image} width="300" />}
+        </>
+      ) : (
+        <p>Later inn...</p>
       )}
-      <p>{event.info || "Ingen ytterligere informasjon tilgjengelig."}</p>
-      <p>
-        <strong>Sted:</strong>{" "}
-        {event._embedded?.venues?.[0]?.name || "Ukjent"}
-      </p>
-    </div>
+    </section>
   );
 }

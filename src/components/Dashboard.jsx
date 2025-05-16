e3dd29a78cb97903d3e34032827abb2c1e636cf0
Martehalv <martehal@hiof.no>
2025-05-16 14:07:44 +0200

    lagt inn kilder og kommentarer

--- src/components/Dashboard.jsx
+++ src/components/Dashboard.jsx
@@ -3,9 +3,10 @@ import { useState } from "react";
 export default function Dashboard() {
   const [loggedIn, isLoggedIn] = useState(false);
 
-  const buttonClick = (e) => {
-    e.preventDefault();
-    const username = e.target.elements.username.value.trim();
+  const buttonClick = (event) => {
+    //https://developer.mozilla.org/en-US/docs/Web/API/HTMLFormElement/submit_event hjuelpet litt med funksjonen.
+    event.preventDefault(); //https://developer.mozilla.org/en-US/docs/Web/API/Event/preventDefault hjelpet med event.preventDefault()
+    const username = event.target.elements.username.value.trim();
     if (username !== "") {
       isLoggedIn(true);
     }
@@ -17,6 +18,8 @@ export default function Dashboard() {
 
       {!loggedIn && (
         <form onSubmit={buttonClick}>
+          {" "}
+          {/*https://www.w3schools.com/jsref/event_onsubmit.asp hjelpet med onsubmit. */}
           <label>Brukernavn: </label>
           <input type="text" name="username" />
           <button type="submit">Logg inn</button>
